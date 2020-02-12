098438b934ceada52e44b2ef33edd5cfbc1628f4
Fabio <fabiomarsiaj@gmail.com>
2020-02-12 15:31:25 -0300

    Category domain created.

--- src/main/java/com/fabioqmarsiaj/estore/resources/CategoryResource.java
+++ src/main/java/com/fabioqmarsiaj/estore/resources/CategoryResource.java
@@ -1,15 +1,26 @@
 package com.fabioqmarsiaj.estore.resources;
 
+import com.fabioqmarsiaj.estore.domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String list(){
-        return "REST is working..";
+    public List<Category> list(){
+
+        Category informatics = new Category(1, "Informatics");
+        Category office = new Category(2, "Office");
+
+        List<Category> categories = new ArrayList<>();
+        categories.add(informatics);
+        categories.add(office);
+        return categories;
     }
 }
