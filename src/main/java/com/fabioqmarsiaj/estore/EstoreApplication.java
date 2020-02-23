9d9e373538b277f8087df4255084a068d6bd40c7
Fabio Marsiaj <fabiomarsiaj@gmail.com>
2020-02-23 15:52:54 -0300

    Instanciate models with repository

--- src/main/java/com/fabioqmarsiaj/estore/EstoreApplication.java
+++ src/main/java/com/fabioqmarsiaj/estore/EstoreApplication.java
@@ -1,13 +1,30 @@
 package com.fabioqmarsiaj.estore;
 
+import com.fabioqmarsiaj.estore.domain.Category;
+import com.fabioqmarsiaj.estore.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class EstoreApplication {
+public class EstoreApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(EstoreApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Informatics");
+        Category cat2 = new Category(null, "Office");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
