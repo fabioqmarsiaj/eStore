f63d866a9bb7191db0f4a13000a245433ef80a64
Fabio <fabiomarsiaj@gmail.com>
2020-05-28 16:50:02 -0300

    Customize return validation error

--- src/main/java/com/fabioqmarsiaj/estore/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/fabioqmarsiaj/estore/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.fabioqmarsiaj.estore.services.exceptions.CategoryNotFoundException;
 import com.fabioqmarsiaj.estore.services.exceptions.DataIntegrityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import javax.servlet.http.HttpServletRequest;
@@ -23,5 +25,19 @@ public class ResourceExceptionHandler {
 
         StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validations(MethodArgumentNotValidException e, HttpServletRequest request){
+
+        ValidationError validationError = new ValidationError(
+                HttpStatus.BAD_REQUEST.value(),"Validation Error", System.currentTimeMillis()
+        );
+
+        e.getBindingResult().getFieldErrors().forEach(fieldError ->
+                validationError.addError(fieldError.getField(), fieldError.getDefaultMessage())
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(validationError);
     }
 }
